e4cbb939133a6f8c56e66cb4ee1057cd1faff91b
joze-net <50345325+joze-net@users.noreply.github.com>
2019-10-05 20:17:30 -0500

    el proyecto quedo funcional se puede dibujar pefectamente

--- eventoTeclas.js
+++ eventoTeclas.js
@@ -1,6 +1,28 @@
-document.addEventListener(/*"keydown"*/"keyup",dibujar);
+document.addEventListener(/*"keydown"*/"keydown",dibujar);
+var btn=document.getElementById("limpiar");
+btn.addEventListener("click",limpiar);
+var d=document.getElementById('dibujito');
+var lienzo=d.getContext("2d");
+
+
+
+var xinicial=150;
+var yinicial=150;
+var xfinal=150;
+var yfinal=150;
+
+function limpiar(){
+	lienzo.clearRect(0,0,d.width,d.height);
+	xinicial=150;
+	yinicial=150;
+	xfinal=150;
+	yfinal=150;
+
+}
 
 function dibujar(evento){
+
+	
 
 	var letras = {
 		UP: 38,
@@ -8,28 +30,87 @@ function dibujar(evento){
 		LEFT: 37,
 		RIGHT: 39
 	}
+    
+   
 
+	
+    
 	switch (evento.keyCode) {
 		case letras.UP:
-			console.log("voy arriba");
+			
+			if(yfinal>0){
+				yfinal=yfinal-10;
+			hacerLinea(xinicial,yinicial,xfinal,yfinal);
+			yinicial=yfinal;
+			}else{
+				alert("llegaste al limite");
+			}
+
+			
+						
 			break;
 
 		case letras.DOWN:
-			console.log("voy abajo");
+			
+			if(yfinal<d.height){
+				yfinal=yfinal+10;
+			    hacerLinea(xinicial,yinicial,xfinal,yfinal);
+			    yinicial=yfinal;
+			}else{
+				alert("llegaste al limite");
+			}
+			
 			break;
 
 		case letras.LEFT:
-			console.log("voy a la izquierda");
+			
+			if(xfinal>0){
+
+				xfinal=xfinal-10;
+			    hacerLinea(xinicial,yinicial,xfinal,yfinal);
+			    xinicial=xfinal;
+
+			}else {
+				alert("llegaste al limite");
+			}
+			
 			break;
 
 		case letras.RIGHT:
-			console.log("voy a al derecha");
+			
+			if (xfinal<d.width) {
+
+				xfinal=xfinal+10;
+			    hacerLinea(xinicial,yinicial,xfinal,yfinal);
+			    xinicial=xfinal;
+
+			}else {
+				alert("llegaste al limite");
+			}
+			
 			break;
+
+
 		default:
-			console.log("letra diferente");
+			
 			break;
+	}
+}
+
+	
+
+	function hacerLinea(xinicial,yinicial,xfinal,yfinal){
+
+		lienzo.beginPath();
+		lienzo.strokeStyle= "black";
+		lienzo.moveTo(xinicial,yinicial);
+		lienzo.lineTo(xfinal,yfinal);
+		lienzo.stroke();
+		lienzo.closePath();
+       
 	}
 
 
-	console.log(evento.keyCode);
-}
+
+
+	
